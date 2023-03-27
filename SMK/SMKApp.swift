c733c0daf9c295bff41fced84a4a903e1d618e29
leejunghoo515@naver.com <leejunghoo515@naver.com>
2023-03-27 09:35:14 +0900

    Initial Commit

---
+++ SMK/SMKApp.swift
@@ -0,0 +1,17 @@
+//
+//  SMKApp.swift
+//  SMK
+//
+//  Created by 이정후 on 2023/03/27.
+//
+
+import SwiftUI
+
+@main
+struct SMKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
